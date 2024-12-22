b2c23e735c69084ce56f0457c39cc93cf2742153
Khaled Samim <126877550+itkhld1@users.noreply.github.com>
2024-12-22 23:37:27 +0300

    Initial Commit

---
+++ FirstAidGuide/FirstAidGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAidGuideApp.swift
+//  FirstAidGuide
+//
+//  Created by itkhld on 22.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct FirstAidGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
